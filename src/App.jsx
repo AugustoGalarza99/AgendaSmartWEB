58b6bb5c6dd39badd143a812c9880d995a85e297
AugustoGalarza99 <augustogalarza15@gmail.com>
2025-01-21 21:38:47 -0300

    Primer Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { motion } from "framer-motion";
+import "./App.css";
+import HeroSection from "./components/HeroSection/HeroSection";
+
+function App() {
+  return (
+    <div>
+      <HeroSection />
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./HeroSection.css";
+import agendaImage from "../../assets/1.png"; // Asegúrate de colocar tu imagen en la carpeta assets o ajustar la ruta.
+
+function HeroSection() {
+  return (
+    <div className="hero-section">
+      <div className="hero-content">
+        <h1 className="hero-title">
+          Organiza tu día <span>de manera inteligente</span>
+        </h1>
+        <p className="hero-text">
+          Descubre la mejor manera de gestionar tus tareas, eventos y
+          recordatorios con nuestra agenda digital intuitiva y eficiente.
+        </p>
+        <button className="cta-button">Comienza ahora mismo</button>
+      </div>
+      <div className="hero-image">
+        <img
+          src={agendaImage}
+          alt="Presentación de la agenda digital"
+          className="image"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
